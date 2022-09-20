6fef0194b2f1ce20a6489e83fc60ec6553c019d3
William Killett <williamkillettiii@yahoo.com>
2022-09-20 14:45:30 -0700

    commit tree 0b643bdc5b0815d41102be8d40945001059d87ac

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+if ( feet <= 400){
+  result = 'This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+if (feet> 400 && feet <2000){
+  result = 'That will be twenty bucks.'
+}
+if (feet >  2000 && feet < 2500){
+  result = 'I will gladly take your thirty bucks.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+if (feet > 2500){
+  result = 'No can do.'
 }
+return result
+
+}
+
+function ternaryCheckCity(city){
+  let result
+  if (city === 'NYC'){
+    result = "Ok, sounds good."
+  } else {
+    result = "No go."
+}
+  return result
+}
+
+function switchOnCharmFromTip(tip){
+  let result
+ if(tip === "generous"){
+    result = 'Thank you so much.'
+  }
+  else if(tip === "not as generous"){
+    result = 'Thank you.'
+  } else {
+    result = "Bye."
+  }
+  return result
+}
+
